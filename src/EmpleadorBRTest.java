15ddf53bce05b46f3ca4e7c41f9c93ee3245d959
Isanvil <isaac.isanvil@gmail.com>
2024-03-05 13:51:58 +0100

    "Test 6 al 10"

--- src/EmpleadorBRTest.java
+++ src/EmpleadorBRTest.java
@@ -32,4 +32,35 @@ class EmpleadorBRTest {
         EmpleadorBR e = new EmpleadorBR();
         assertEquals(840, e.calculaSalarioNeto(e.setSalarioNeto((float) 1000)), 0.01);
     }
+
+    // SEGONA TANDA
+    @Test
+    public void testSal999_99Ei999_99(){
+        EmpleadorBR e = new EmpleadorBR();
+        assertEquals(999.99, e.calculaSalarioNeto(e.setSalarioNeto((float) 999.99)), 0.01);
+    }
+
+    @Test
+    public void testSal500Ei500(){
+        EmpleadorBR e = new EmpleadorBR();
+        assertEquals(500, e.calculaSalarioNeto(e.setSalarioNeto((float) 500)), 0.01);
+    }
+
+    @Test
+    public void testSal0Ei0(){
+        EmpleadorBR e = new EmpleadorBR();
+        assertEquals(0, e.calculaSalarioNeto(e.setSalarioNeto((float) 0)), 0.01);
+    }
+
+    @Test
+    public void testSalMenos1EiMenos1(){
+        EmpleadorBR e = new EmpleadorBR();
+        assertEquals(-1, e.calculaSalarioNeto(e.setSalarioNeto((float) -1)), 0.01);
+    }
+
+    @Test
+    public void testSalVen2000E8HEi1360(){
+        EmpleadorBR e = new EmpleadorBR();
+        assertEquals(1360, e.calculaSalarioBruto(TipoEmpleado.venedor, 2000, 8));
+    }
 }
